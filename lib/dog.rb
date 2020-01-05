d9c243abdc99d8f48f99c37ed74c4544753265a7
Taryn Johnson <taryn.johnson919@gmail.com>
2020-01-05 22:17:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Add your code here
+class Dog 
+
+attr_accessor :name   
+  
+  
+@@dog_certificates = []
+  
+def initialize (name)
+  @name = name 
+  @@dog_certificates << self 
+end 
+  
+def self.all 
+  @@dog_certificates
+end 
+  
+end 
